f3da195a6068f4cb3ec3b837b030e157319dd37f
Michelle Horn <michellehorn@live.com>
2017-09-05 21:38:23 -0300

    Add files via upload

---
+++ JS4Girls_js/2etapaAgostoJS4girls/logica-js/assets/js/jquery-test.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".info-group a").click(function(){
+    	console.log("entrou");
+        $(".info-group a").slideToggle();
+    });
+
+ //    $("p").hover(function(){
+	//     $(this).css("background-color", "pink");
+	// });
+});
